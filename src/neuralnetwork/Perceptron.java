70eb2ffe423d03ea09c5f05bee9632022ecc20e9
SlicSide <rico_hahn@web.de>
2017-07-31 15:42:39 +0200

    InitialCommit

---
+++ src/neuralnetwork/Perceptron.java
@@ -0,0 +1,35 @@
+package neuralnetwork;
+
+public class Perceptron {
+    public static final double THRESHOLD = 1.0;
+    public static final int[][][] trainingData = {{{0,0},{0}},
+                                                  {{0,1},{0}},
+                                                  {{1,0},{0}},
+                                                  {{1,1},{1}}};
+    public static final double LEARNING_RATE = 0.05;
+    public static final double[] INITIAL_WEIGHTS = {Math.random(),Math.random()};
+
+    public double calculateWeightedSum(int[] data, double[] weights){
+        double weightedSum=0;
+        for(int i=0;i<data.length;i++){
+            weightedSum+=data[i]*weights[i];
+        }
+        return weightedSum;
+    }
+
+    public int applyActivationFunction(double weightedSum) {
+        int result = 0;
+        if(weightedSum > THRESHOLD) {
+            result = 1;
+        }
+        return result;
+    }
+
+    public double[] adjustWeights(int[] data, double[] weights, double error) {
+        double[] adjustedWeights = new double[weights.length];
+        for(int i=0; i < weights.length; i++) {
+            adjustedWeights[i] = LEARNING_RATE * error * data[i] + weights[i];
+        }
+        return adjustedWeights;
+    }
+}
